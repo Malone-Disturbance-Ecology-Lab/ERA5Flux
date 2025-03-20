d2ddcacbfb1cc20bf4520ef1ca8b26b516506522
Ammara Talib <talib@wisc.edu>
2025-03-20 14:42:23 -0400

    updated_blend_merge

--- 03_merge_ERA5_FLUX.R
+++ 03_merge_ERA5_FLUX.R
@@ -1,3 +1,4 @@
+rm(list = ls())  # Removes all objects from the environment
 ########################################
 ## this script is used to merge data from AmeriFlux and data from ERA5 by adding new columns to AmeriFlux file
 ## Authors: Ammara Talib and Junna Wang
@@ -12,107 +13,142 @@
 library(librarian)
 shelf('amerifluxr', 'tidyr', 'lubridate')
 
+
+filename_FLUX <- 'data_merge/AMF_US-EvM_BASE-BADM_2-5.zip'
+filename_ERA5 <- 'data_merge/US-EvM_ERA_2020_2023_hr.csv'
+varname_FLUX <- c('SW_IN')
+varname_ERA5 <- c('ssrd')
+
+
 merge_ERA5_FLUX <- function(filename_FLUX, filename_ERA5,
-                            varname_FLUX, varname_ERA5,
-                            blending_rules) {
-  
-  ########testing the code
-  filename_FLUX <- 'data_merge/AMF_BR-Sa1_BASE-BADM_5-5.zip'
-  filename_ERA5 <- 'data_merge/BR-Sa1_tp_2002_2011.csv'
-  varname_FLUX <- c('P')
-  varname_ERA5 <- c('tp')
-  blending_rules <- c('replace')
-  #
-  #
+                            varname_FLUX, varname_ERA5) {
   if (length(filename_FLUX) > 1 | length(filename_ERA5) > 1) {
-    stop('this function works for one site each time.')
+    stop('This function works for one site each time.')
   }
-  #
   if (length(varname_FLUX) != length(varname_ERA5)) {
-    stop('to-be-merged varname of FLUX should correspond to to-be-merged varname of ERA5')
+    stop('To-be-merged varname of FLUX should correspond to to-be-merged varname of ERA5')
   } 
-  #
-  # we also need to make sure varname_ERA5 is the same in the climate data center welsite. 
   
+  # Read FLUX data
+  data_BASE <- amf_read_base(filename_FLUX, parse_timestamp = TRUE, unzip = TRUE)
+  data_BASE[data_BASE <= -9999] <- NA  # Replace abnormal values with NA
   
-  #
-  # read amf
-  data_BASE <- amf_read_base(filename_FLUX, parse_timestamp=TRUE, unzip = T)
-  # set abnormal values (<=-9999) to NA
-  data_BASE[data_BASE<=-9999] <- NA
-  
-  # read era5
+  # Read ERA5 data
   data_ERA5 <- read.csv(filename_ERA5)
   
-  # check if variable names are in the data
-  # it could be no such variables in the data
-  # if (! varname_FLUX %in% colnames(data_BASE)) {
-  #   stop('wrong varnames were given for FLUX data')
-  # }
-  #
-  if (! varname_ERA5 %in% colnames(data_ERA5)) {
-    stop('wrong varnames were given for ERA5 data')
+  if (!varname_ERA5 %in% colnames(data_ERA5)) {
+    stop('Wrong varnames were given for ERA5 data')
   }
   
-  # find the Ameriflux time step
-  dt <- as.numeric(difftime(ymd_hm(data_BASE$TIMESTAMP_END[2]), ymd_hm(data_BASE$TIMESTAMP_END[1]), units=c('hours')))
-  
-  # convert time column to real time
+  # Convert time column to datetime format
   data_ERA5$time <- ymd_hm(data_ERA5$time)
   
-  # interpolate ERA5 to half hourly
+  # Find time step of Ameriflux data
+  dt <- as.numeric(difftime(ymd_hm(data_BASE$TIMESTAMP_END[2]), ymd_hm(data_BASE$TIMESTAMP_END[1]), units = 'hours'))
+  
+  # Interpolate ERA5 data to half-hourly intervals
   if (dt == 0.5) {
-    data_ERA5_intp <- data.frame(time=seq(data_ERA5$time[1], data_ERA5$time[nrow(data_ERA5)] + 30*60, by = "30 min"))
-    # this is related to variables. 
+    data_ERA5_intp <- data.frame(time = seq(data_ERA5$time[1], data_ERA5$time[nrow(data_ERA5)] + 30*60, by = "30 min"))
+    
     for (i in 1:length(varname_ERA5)) {
       if (varname_ERA5[i] == 'tp') {
-        data_ERA5_intp[, i+1] <- rep(data_ERA5[, varname_ERA5[i]], each=2)
+        data_ERA5_intp[, i + 1] <- rep(data_ERA5[, varname_ERA5[i]], each = 2)
       } else {
-        data_ERA5_intp[, i+1] <- approx(data_ERA5$time, data_ERA5[, varname_ERA5[i]], data_ERA5_intp$time, method = "linear")
+        data_ERA5_intp[, i + 1] <- approx(data_ERA5$time, data_ERA5[, varname_ERA5[i]], data_ERA5_intp$time, method = "linear")$y
       }
     }
+    colnames(data_ERA5_intp)[-1] <- varname_ERA5
   } else {
     data_ERA5_intp <- data_ERA5
   }
-  # we will use data_ERA5_intp in the following. 
   
-  # Merge the variables using different rules
-  df_merge_ERA5_FLUX <- data_BASE  # Start with base dataset
+  # Format time and add TIMESTAMP for data_BASE
+  df_result <- data_ERA5_intp
+  df_result$time <- format(df_result$time, "%Y-%m-%d %H:%M:%S")
+  data_BASE$TIMESTAMP <- ymd_hm(as.character(data_BASE$TIMESTAMP_START))
+  data_BASE$time <- format(data_BASE$TIMESTAMP, "%Y-%m-%d %H:%M:%S")
   
-  for (i in 1:length(varname_FLUX)) {
-    flux_var <- varname_FLUX[i]
-    era_var <- varname_ERA5[i]
+  # Merge df_result and varname_FLUX variable from data_BASE
+  merged_data <- merge(df_result, data_BASE[, c("time", varname_FLUX)], by = "time", all = TRUE)
+  
+  # Return the merged data
+  return(merged_data)
+}
+
+# Example function call
+df_result <- merge_ERA5_FLUX(filename_FLUX, filename_ERA5, c('SW_IN'), c('ssrd'))
+
+merged_data=df_result
+
+### apply blending function on merged data 
+
+blend_ERA5_FLUX <- function(merged_data, varname_FLUX, varname_ERA5, blending_rule) {
+  message("Using columns: ", varname_FLUX, " and ", varname_ERA5)
+  
+  if (blending_rule == "replace") {
+    merged_data[[paste0(varname_FLUX, "_replace")]] <- merged_data[[varname_ERA5]]
+  }
+  
+  if (blending_rule == "lm") {
+    complete_cases <- merged_data[!is.na(merged_data[[varname_FLUX]]) & !is.na(merged_data[[varname_ERA5]]), ]
     
-    if (blending_rules == "replace") {
-      df_merge_ERA5_FLUX[[paste0(flux_var, "_replace")]] <- data_ERA5_intp[[era_var]]
-    }
-    
-    if (blending_rules == "lm") {
-      lm_model <- lm(data_ERA5_intp[[era_var]] ~ df_merge_ERA5_FLUX[[flux_var]])
-      df_merge_ERA5_FLUX[[paste0(flux_var, "_lm")]] <- predict(lm_model, newdata = df_merge_ERA5_FLUX)
-    }
-    
-    if (blending_rules == "lm_no_intercept") {
-      lm_no_intercept_model <- lm(data_ERA5_intp[[era_var]] ~ df_merge_ERA5_FLUX[[flux_var]] - 1)
-      df_merge_ERA5_FLUX[[paste0(flux_var, "_lm_no_intercept")]] <- predict(lm_no_intercept_model, newdata = df_merge_ERA5_FLUX)
-    }
-    
-    if (blending_rules == "automatic") {
-      lm_auto <- lm(data_ERA5_intp[[era_var]] ~ df_merge_ERA5_FLUX[[flux_var]])
-      predicted_values <- predict(lm_auto, newdata = df_merge_ERA5_FLUX)
-      filled_values <- ifelse(is.na(df_merge_ERA5_FLUX[[flux_var]]), predicted_values, df_merge_ERA5_FLUX[[flux_var]])
-      df_merge_ERA5_FLUX[[paste0(flux_var, "_automatic")]] <- filled_values
+    if (nrow(complete_cases) > 1) {  
+      formula_str <- paste(varname_ERA5, "~", varname_FLUX)
+      message("Fitting linear model with formula: ", formula_str)
+      lm_model <- lm(as.formula(formula_str), data = complete_cases)
+      
+      merged_data[[paste0(varname_FLUX, "_lm")]] <- predict(lm_model, 
+                                                            newdata = setNames(data.frame(merged_data[[varname_FLUX]]), varname_FLUX))
+      
+      merged_data[[paste0(varname_FLUX, "_lm")]][is.na(merged_data[[paste0(varname_FLUX, "_lm")]])] <- 
+        predict(lm_model, newdata = setNames(data.frame(mean(complete_cases[[varname_FLUX]], na.rm = TRUE)), varname_FLUX))
+    } else {
+      merged_data[[paste0(varname_FLUX, "_lm")]] <- merged_data[[varname_FLUX]]
     }
   }
   
-  return(df_merge_ERA5_FLUX)
+  if (blending_rule == "lm_no_intercept") {
+    complete_cases <- merged_data[!is.na(merged_data[[varname_FLUX]]) & !is.na(merged_data[[varname_ERA5]]), ]
+    
+    if (nrow(complete_cases) > 1) {  
+      formula_str <- paste(varname_ERA5, "~", varname_FLUX, "+ 0")
+      message("Fitting linear model without intercept with formula: ", formula_str)
+      lm_model_no_intercept <- lm(as.formula(formula_str), data = complete_cases)
+      
+      merged_data[[paste0(varname_FLUX, "_lm_no_intercept")]] <- predict(lm_model_no_intercept, 
+                                                                         newdata = setNames(data.frame(merged_data[[varname_FLUX]]), varname_FLUX))
+      
+      merged_data[[paste0(varname_FLUX, "_lm_no_intercept")]][is.na(merged_data[[paste0(varname_FLUX, "_lm_no_intercept")]])] <- 
+        predict(lm_model_no_intercept, newdata = setNames(data.frame(mean(complete_cases[[varname_FLUX]], na.rm = TRUE)), varname_FLUX))
+    } else {
+      merged_data[[paste0(varname_FLUX, "_lm_no_intercept")]] <- merged_data[[varname_FLUX]]
+    }
+  }
+  
+  if (blending_rule == "automatic") {
+    na_rows <- is.na(merged_data[[varname_FLUX]])
+    mean_FLUX <- mean(merged_data[[varname_FLUX]], na.rm = TRUE)
+    
+    na_subset <- merged_data[na_rows, ]
+    na_subset[[varname_FLUX]] <- mean_FLUX
+    
+    if (exists("lm_model")) {  # Ensure lm_model exists
+      predicted_values <- predict(lm_model, newdata = setNames(data.frame(na_subset[[varname_FLUX]]), varname_FLUX))
+      merged_data[[paste0(varname_FLUX, "_automatic")]] <- merged_data[[varname_FLUX]]
+      merged_data[[paste0(varname_FLUX, "_automatic")]][na_rows] <- predicted_values
+    } else {
+      message("Linear model not available for 'automatic' blending. Keeping original values.")
+      merged_data[[paste0(varname_FLUX, "_automatic")]] <- merged_data[[varname_FLUX]]
+    }
+  }
+  
+  return(merged_data)
 }
 
 
+merg_blend <- blend_ERA5_FLUX(merged_data, 'SW_IN', 'ssrd', blending_rule = "lm")
 
-df_result <- merge_ERA5_FLUX('data_merge/AMF_BR-Sa1_BASE-BADM_5-5.zip',
-                             'data_merge/BR-Sa1_tp_2002_2011.csv',
-                             c('P'),
-                             c('tp'),
-                             "lm")
-print(head(df_result))
+
+write.csv(merg_blend , "merg_blend.csv", row.names = FALSE)
+
+
