b42d4e819b8a68b54ca3c70617fe724c3eb0536e
Angel Chen <angelchen7777@yahoo.com>
2025-07-25 17:54:56 -0700

    fix: remove `rm(list=ls())` call from day_padding.R

--- R/day_padding.R
+++ R/day_padding.R
@@ -1,25 +1,25 @@
-# 
+#
 ##' .. this function is designed to pad 1 day for the beginning and ending day. ..
 ##'
 ##' @title day_padding, this function will produce a new starting and ending day.
 ##' @param input start and end day
 ##' @return new start and end day
-##' 
-##' @example 
+##'
+##' @example
 ##' result <- day_padding("2024-01-01", "2025-12-31")
 ##' @author Boya ("Paul") Zhang
 
-##' 
-rm(list=ls())
+##'
+
 day_padding <- function(start_day, end_day) {
   # Convert string to Date format
   start_date <- as.Date(start_day)
   end_date <- as.Date(end_day)
-  
+
   # Adjust dates
   new_start <- start_date - 1
   new_end <- end_date + 1
-  
+
   # Return as character format
   return(list(start_day = as.character(new_start), end_day = as.character(new_end)))
 }
