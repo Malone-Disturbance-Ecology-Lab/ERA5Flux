ab5c02393aea7558b9ba5db8a1e9d7560e936bc9
Angel Chen <angelchen7777@yahoo.com>
2025-07-29 16:31:52 -0700

    feat: edited documentation, formatting, namespacing, error messages, and comments for day_padding.R

--- R/day_padding.R
+++ R/day_padding.R
@@ -1,26 +1,36 @@
-#' .. this function is designed to pad 1 day for the beginning and ending day. ..
+#' @title Pad Dates by 1 Day
 #'
-#' @title day_padding, this function will produce a new starting and ending day.
-#' @param input start and end day
-#' @return new start and end day
+#' @description
+#' This function is designed to pad 1 day for the beginning and ending days. It will add 1 day to start and end dates for future time zone offset issues.
+#'
+#' @param start_day (character) Start date in either %Y-%m-%d or %Y/%m/%d format.
+#' @param end_day (character) End date in either %Y-%m-%d or %Y/%m/%d format.
+#' @return (character) New start and end dates in either %Y-%m-%d or %Y/%m/%d format, padded by 1 day each.
+#'
 #' @export
 #'
 #' @examples
+#' # Pad starting and ending dates by 1 day
 #' result <- day_padding("2024-01-01", "2025-12-31")
+#'
 #' @author Boya ("Paul") Zhang
 #'
+day_padding <- function(start_day = NULL,
+                        end_day = NULL) {
+  # Error out if no start day is provided
+  if (base::is.null(start_day)) stop("No start day provided")
 
-day_padding <- function(start_day, end_day) {
+  # Error out if no end day is provided
+  if (base::is.null(end_day)) stop("No end day provided")
+
   # Convert string to Date format
-  start_date <- as.Date(start_day)
-  end_date <- as.Date(end_day)
+  start_date <- base::as.Date(start_day)
+  end_date <- base::as.Date(end_day)
 
   # Adjust dates
   new_start <- start_date - 1
   new_end <- end_date + 1
 
   # Return as character format
-  return(list(start_day = as.character(new_start), end_day = as.character(new_end)))
+  return(base::list(start_day = base::as.character(new_start), end_day = base::as.character(new_end)))
 }
-
-result <- day_padding("2024-01-01", "2025-12-31")
